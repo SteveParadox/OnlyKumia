b231849d1e3c3ed62708e75c8de220509e352e42
SteveParadox <fordstphn@gmail.com>
2025-11-07 11:23:35 +0000

    edit firebase config

--- zeevx/src/Auth/Firebase.js
+++ zeevx/src/Auth/Firebase.js
@@ -8,7 +8,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyAR1J45p0ePpVqZzz93X7JGiSRy-1Seryg",
   authDomain: "onlykumia.firebaseapp.com",
   projectId: "onlykumia",
-  storageBucket: "onlykumia.firebasestorage.app",
+  storageBucket: "onlykumia.appspot.com",
   messagingSenderId: "674830747175",
   appId: "1:674830747175:web:b7e28ee399d40ec6ef7002",
   measurementId: "G-M283T6N2W7"
