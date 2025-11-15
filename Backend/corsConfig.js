b997e35501ccc68537876fb4758553e500d1c4fc
xavenordu <xavenordu@gmail.com>
2025-11-15 08:31:48 +0100

    Update corsConfig.js

--- Backend/corsConfig.js
+++ Backend/corsConfig.js
@@ -1,11 +1,10 @@
 // corsConfig.js
 
 const corsOptions = {
-    origin: 'http://localhost:3000',
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    credentials: true,
-    optionsSuccessStatus: 204,
-  };
+  origin: [/\.app\.github\.dev$/, 'http://localhost:3000', 'http://localhost:8001'],
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true
+}
   
-  export default corsOptions;
-  
+export default corsOptions;
